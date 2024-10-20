2dedc8d1099e0b4d3e507c0049ee9bdcf12d77f0
Edwin Moquete <edwinmmoquete@gmail.com>
2024-10-20 15:56:34 +0000

    enchilada: Setup ELF checks
    Change-Id: Iacc8a942f4e09bf61f5b8e0f4ad8e70f0b43dfa8

--- enchilada-vendor.mk
+++ enchilada-vendor.mk
@@ -16,8 +16,6 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/enchilada/proprietary/odm/etc/acdbdata/MTP/MTP_Speaker_cal.acdb:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MTP/MTP_Speaker_cal.acdb \
     vendor/oneplus/enchilada/proprietary/odm/etc/acdbdata/MTP/MTP_workspaceFile.qwsp:$(TARGET_COPY_OUT_ODM)/etc/acdbdata/MTP/MTP_workspaceFile.qwsp \
     vendor/oneplus/enchilada/proprietary/odm/etc/odm_feature_list:$(TARGET_COPY_OUT_ODM)/etc/odm_feature_list \
-    vendor/oneplus/enchilada/proprietary/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.oneplus:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.oneplus \
-    vendor/oneplus/enchilada/proprietary/vendor/bin/hw/android.hardware.keymaster@3.0-service-qti:$(TARGET_COPY_OUT_VENDOR)/bin/hw/android.hardware.keymaster@3.0-service-qti \
     vendor/oneplus/enchilada/proprietary/vendor/etc/firmware/tp/fw_synaptics_17819.img:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/tp/fw_synaptics_17819.img \
     vendor/oneplus/enchilada/proprietary/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.oneplus.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.biometrics.fingerprint@2.1-service.oneplus.rc \
     vendor/oneplus/enchilada/proprietary/vendor/etc/init/android.hardware.keymaster@3.0-service-qti.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/android.hardware.keymaster@3.0-service-qti.rc \
@@ -30,18 +28,20 @@ PRODUCT_COPY_FILES += \
     vendor/oneplus/enchilada/proprietary/vendor/lib/camera/oneplus6_vstab_db_0_4k_video_30fps.config:$(TARGET_COPY_OUT_VENDOR)/lib/camera/oneplus6_vstab_db_0_4k_video_30fps.config \
     vendor/oneplus/enchilada/proprietary/vendor/lib/camera/oneplus6_vstab_db_0_720p_video_30fps.config:$(TARGET_COPY_OUT_VENDOR)/lib/camera/oneplus6_vstab_db_0_720p_video_30fps.config \
     vendor/oneplus/enchilada/proprietary/vendor/lib/camera/oneplus6_vstab_db_1_1080p_video_30fps.config:$(TARGET_COPY_OUT_VENDOR)/lib/camera/oneplus6_vstab_db_1_1080p_video_30fps.config \
-    vendor/oneplus/enchilada/proprietary/vendor/lib/camera/oneplus6_vstab_db_1_720p_video_30fps.config:$(TARGET_COPY_OUT_VENDOR)/lib/camera/oneplus6_vstab_db_1_720p_video_30fps.config \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/com.fingerprints.extension@1.0.so:$(TARGET_COPY_OUT_VENDOR)/lib64/com.fingerprints.extension@1.0.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/fp_hal_extension.so:$(TARGET_COPY_OUT_VENDOR)/lib64/fp_hal_extension.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/fpc_hal_common.so:$(TARGET_COPY_OUT_VENDOR)/lib64/fpc_hal_common.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/fpc_hal_extension.so:$(TARGET_COPY_OUT_VENDOR)/lib64/fpc_hal_extension.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/fpc_tac.so:$(TARGET_COPY_OUT_VENDOR)/lib64/fpc_tac.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/hw/fingerprint.sdm845.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.sdm845.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/hw/goodix.fingerprint.sdm845.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/goodix.fingerprint.sdm845.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/libgf_ca.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_ca.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/libgf_hal.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgf_hal.so \
-    vendor/oneplus/enchilada/proprietary/vendor/lib64/libgoodixfingerprintd_binder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libgoodixfingerprintd_binder.so
+    vendor/oneplus/enchilada/proprietary/vendor/lib/camera/oneplus6_vstab_db_1_720p_video_30fps.config:$(TARGET_COPY_OUT_VENDOR)/lib/camera/oneplus6_vstab_db_1_720p_video_30fps.config
 
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.oneplus.xml
+    com.fingerprints.extension@1.0 \
+    fp_hal_extension \
+    fpc_hal_common \
+    fpc_hal_extension \
+    fpc_tac \
+    android.hardware.keymaster@3.0-impl-qti \
+    fingerprint.sdm845 \
+    goodix.fingerprint.sdm845 \
+    libgf_ca \
+    libgf_hal \
+    libgoodixfingerprintd_binder \
+    android.hardware.biometrics.fingerprint@2.1-service.oneplus.xml \
+    android.hardware.biometrics.fingerprint@2.1-service.oneplus \
+    android.hardware.keymaster@3.0-service-qti
